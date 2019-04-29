7062c9d95ffe0ff2e9ffb4157a1f9c5998e4aee4
LiliGao <1043575262@qq.com>
2019-04-29 17:13:13 +0800

    第一个git项目

---
+++ frist_GIT/src/com/itheima/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+
+        System.out.println("nihao ");
+    }
+}
