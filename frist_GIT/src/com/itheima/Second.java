96a90e6e13f4a1b747de5440bd992bce3bdbe438
LiliGao <1043575262@qq.com>
2019-04-29 17:23:52 +0800

    第二个git项目

---
+++ frist_GIT/src/com/itheima/Second.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("second text");
+        System.out.println("second text");
+        System.out.println("second text");
+        System.out.println("second text");
+    }
+
+}
